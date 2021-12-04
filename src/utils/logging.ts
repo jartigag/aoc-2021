60ae367bc5f95d86aaac35f027e839c923ab2055
jartigag <jartigag@pm.me>
2021-12-04 18:58:47 +0100

    format: prettier on utils/

--- src/utils/logging.ts
+++ src/utils/logging.ts
@@ -1,9 +1,9 @@
 export const logAnswer = (answer: any) => {
-  process.stdout.write(`\n\n-----------------
+    process.stdout.write(`\n\n-----------------
 Answer: ${answer}
 -----------------\n\n`);
 };
 
 export const log = (...args: any[]) => {
-  process.stdout.write(`${args.join(', ')}\n\n`);
+    process.stdout.write(`${args.join(', ')}\n\n`);
 };
